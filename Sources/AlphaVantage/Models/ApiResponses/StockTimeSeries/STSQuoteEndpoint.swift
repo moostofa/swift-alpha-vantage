6e0b2ed7c7464f1b08b81188d68c529f03a30550
moostofa <mmdrahinz96@gmail.com>
2023-05-02 10:53:20 +1000

    Include init method to attemp parsing global quote

--- Sources/AlphaVantage/Models/ApiResponses/StockTimeSeries/STSQuoteEndpoint.swift
+++ Sources/AlphaVantage/Models/ApiResponses/StockTimeSeries/STSQuoteEndpoint.swift
@@ -3,6 +3,15 @@ extension ApiResponse.StockTimeSeries {
     public struct STSQuoteEndpoint: Decodable {
         let data: [String: MarketDataGlobalQuote]
         
+                    public init(from decoder: Decoder) throws {
+                let container: KeyedDecodingContainer<GenericCodingKeys> = try decoder.container(keyedBy: GenericCodingKeys.self)
+                for key: GenericCodingKeys in container.allKeys {
+                    data = try container.decode(
+                        [String: MarketDataGlobalQuote].self, forKey: key
+                    )
+                }
+            }
+            
         private enum CodingKeys: String, CodingKey {
           case data = "Global Quote"
         }
